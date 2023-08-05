9b34bde88d613423f18cbd8e1190e51abb03af52
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2023-08-05 13:44:03 -0400

    App.js fetched ItemsData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+import ItemsList  from "./Components/ItemsList/ItemsList"
+
+
+//API call to BACKEND
+const API_URL = 'http://localhost:8888'
+
 
 
 function App() {
+
+//create useState for itemsList data
+const [itemsData, setItemsData] = useState([])
+
+//create a useEffect to load itemsList data 
+useEffect(() => {
+
+  console.log(`<App/> useEffect FIRED`)
+
+  //async request to fetch data
+  async function fetchData() {
+    try{
+      const response = await fetch(`${API_URL}/items`)
+
+      const json = await response.json()
+
+      console.log(`<App/> fetch data`, json)
+
+      const {data} = json
+    }catch (err){
+
+    }
+  }
+  fetchData()
+}, [])
+
+console.log(`<App/> Renders with items ${itemsData}`)
   return (
     <div className="App">
-      <h1>YOU GOT THIS</h1>
+    <ItemsList itemsData={itemsData}/>
     </div>
   );
 }
